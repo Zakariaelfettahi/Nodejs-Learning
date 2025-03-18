e4998145cd3cf13cd076ca1b34d869f5be9bbd04
Zakariaelfettahi <zakariaelfettahi@gmail.com>
2025-03-18 00:08:28 -0400

    basics: console.log

---
+++ Basics/consolelog.js
@@ -0,0 +1,11 @@
+console.log("hello world"); // strings
+console.log(34); // integers
+console.log(true); // bool
+console.log(23=="23") // loose equality
+console.log(23==23)
+console.log(23 === "23") // strict equality
+console.log(23 === 23)
+let name = "zakaria";
+console.log(`Hello ${name}`); // equivalent of Python's f-strings
+
+
